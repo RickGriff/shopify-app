d834fe4c1d4c31ec068411601a21e43e4067f676
Rick G <richkws@gmail.com>
2018-04-05 14:56:56 +0000

    Generate shopify app with API key and credentials

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "dd1b18138a75f4c0111eafb5f0722fc7"
+  config.secret = "2c24f9bbae936f6deaa0d315b92aa843"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
